ef8e03a76a9c7c165c92896ebf7cad6d22469a53
Windless95 <barrerajoelj@gmail.com>
2021-08-29 02:21:08 +0000

    finished function v.02

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["arky", "vildred", "Straze", "Iseria"];
+  let what = ["ate", "stole", "destroyed", "restored"];
+  let whatelse = ["cookies", "fear", "the town", "the world"];
+  let where = ["from home", "from us", "from them", "from change"];
+
+  let who1 = who[Math.floor(Math.random() * 4)];
+  let what1 = what[Math.floor(Math.random() * 4)];
+  let whatelse1 = whatelse[Math.floor(Math.random() * 4)];
+  let where1 = where[Math.floor(Math.random() * 4)];
+
+  console.log(who1 + " " + what1 + " " + whatelse1 + " " + where1);
 };
